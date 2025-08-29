db6cf233108fa4d49d068397c1e2707d4bdfd5e0
juanmagarciat <garciajuanmartin258@gmail.com>
2025-08-28 21:22:20 -0300

    Valido todas las variables

--- CONDICIONALES/ejericicios_clase.py
+++ CONDICIONALES/ejericicios_clase.py
@@ -1,3 +1,17 @@
 nombre = input("INGRESE SU NOMBRE: ")
 apellido = input("INGRESE SU APELLIDO: ")
-edad = input("INGRESE SU EDAD: ")
+edad = int(input("INGRESE SU EDAD: "))
+promedio_general = float(input("INGRESE EL PROMEDIO GENERAL: "))
+ingresos_familiares = float(input("INGRESE LOS INGRESOS FAMILIARES MENSUALES: "))
+validez = True
+if edad <= 0 or edad >100:
+    validez = False
+    print('ERROR: INGRESE UNA EDAD VALIDA')
+if promedio_general < 0 or promedio_general > 10:
+    validez = False
+    print('ERROR: INGRESE UN PROMEDIO VALIDO')
+if ingresos_familiares < 0:
+    validez = False
+    print('ERROR: INGRESE UN PROMEDIO VALIDO')
+
+
